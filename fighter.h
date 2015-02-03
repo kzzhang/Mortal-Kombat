6369d09c9f749acfa577b5ce9ae7ed9f165d21e1
zxsiun <kzhang2100@gmail.com>
2015-02-02 19:57:07 -0500

    add associated files

---
+++ fighter.h
@@ -0,0 +1,35 @@
+/* Allegro datafile object indexes, produced by grabber v4.2.0, MSVC.s */
+/* Datafile: c:\Users\Amirthan\Documents\School\Grade 11\ICS3U\Mortal Kombat\fighter.dat */
+/* Date: Tue Jan 13 08:50:44 2015 */
+/* Do not hand edit! */
+
+#define f_Stand_right                    0        /* BMP  */
+#define f_Stand_left                     1        /* BMP  */
+#define f_Walk_right                     2        /* BMP  */
+#define f_Walk_left                      3        /* BMP  */
+#define f_Jump_right                     4        /* BMP  */
+#define f_Jump_left                      5        /* BMP  */
+#define f_Punch_1_right                  6        /* BMP  */
+#define f_Punch_1_left                   7        /* BMP  */
+#define f_Punch_2_right                  8        /* BMP  */
+#define f_Punch_2_left                   9        /* BMP  */
+#define f_Punch_3_right                  10       /* BMP  */
+#define f_Punch_3_left                   11       /* BMP  */
+#define f_Combo_1_right                  12       /* BMP  */
+#define f_Combo_1_left                   13       /* BMP  */
+#define f_Combo_2_right                  14       /* BMP  */
+#define f_Combo_2_left                   15       /* BMP  */
+#define f_Kick_right                     16       /* BMP  */
+#define f_Kick_left                      17       /* BMP  */
+#define f_Special_right                  18       /* BMP  */
+#define f_Special_left                   19       /* BMP  */
+#define f_JumpPunch_right                20       /* BMP  */
+#define f_JumpPunch_left                 21       /* BMP  */
+#define f_JumpKick_right                 22       /* BMP  */
+#define f_JumpKick_left                  23       /* BMP  */
+#define f_Stun_right                     24       /* BMP  */
+#define f_Stun_left                      25       /* BMP  */
+#define f_Block_right                    26       /* BMP  */
+#define f_Block_left                     27       /* BMP  */
+#define f_COUNT                          28
+
---
+++ mage.h
@@ -0,0 +1,41 @@
+/* Allegro datafile object indexes, produced by grabber v4.2.0, MSVC.s */
+/* Datafile: c:\Users\Amirthan\Documents\School\Grade 11\ICS3U\Mortal Kombat\mage.dat */
+/* Date: Tue Jan 13 08:51:15 2015 */
+/* Do not hand edit! */
+
+#define m_Stand_right                    0        /* BMP  */
+#define m_Stand_left                     1        /* BMP  */
+#define m_Walk_right                     2        /* BMP  */
+#define m_Walk_left                      3        /* BMP  */
+#define m_Jump_1_right                   4        /* BMP  */
+#define m_Jump_1_left                    5        /* BMP  */
+#define m_Jump_2_right                   6        /* BMP  */
+#define m_Jump_2_left                    7        /* BMP  */
+#define m_Attack1_right                  8        /* BMP  */
+#define m_Attack1_left                   9        /* BMP  */
+#define m_Attack2_right                  10       /* BMP  */
+#define m_Attack2_left                   11       /* BMP  */
+#define m_SpecPunch1_right               12       /* BMP  */
+#define m_SpecPunch1_left                13       /* BMP  */
+#define m_SpecPunch2_right               14       /* BMP  */
+#define m_SpecPunch2_left                15       /* BMP  */
+#define m_Kick_1_right                   16       /* BMP  */
+#define m_Kick_1_left                    17       /* BMP  */
+#define m_Kick_2_right                   18       /* BMP  */
+#define m_Kick_2_left                    19       /* BMP  */
+#define m_Special_1_right                20       /* BMP  */
+#define m_Special_1_left                 21       /* BMP  */
+#define m_Special_2_right                22       /* BMP  */
+#define m_Special_2_left                 23       /* BMP  */
+#define m_JumpPunch1_right               24       /* BMP  */
+#define m_JumpPunch1_left                25       /* BMP  */
+#define m_JumpPunch2_right               26       /* BMP  */
+#define m_JumpPunch2_left                27       /* BMP  */
+#define m_Jump_Kick_right                28       /* BMP  */
+#define m_Jump_Kick_left                 29       /* BMP  */
+#define m_stun_right                     30       /* BMP  */
+#define m_stun_left                      31       /* BMP  */
+#define m_block_right                    32       /* BMP  */
+#define m_block_left                     33       /* BMP  */
+#define m_COUNT                          34
+
